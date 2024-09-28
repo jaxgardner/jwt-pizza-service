9cbbfaf30a7f181ce21c139c3b8eaa2e1455a5f8
Jaxon Gardner <pjaxongardner@gmail.com>
2024-09-27 21:11:45 -0600

    lint test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const config = require('./config.js');
   try {
     const app = await createApp(config);
 
+    const badVariable = 5
+
     const port = process.argv[2] || 3000;
     app.listen(port, () => {
       console.log(`Server started on port ${port}`);
