cc2684825568c0a9d0adebdbc9386bedc1cbf463
Jaxon Gardner <pjaxongardner@gmail.com>
2024-09-27 21:12:28 -0600

    fix linting

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const config = require('./config.js');
   try {
     const app = await createApp(config);
 
-    const badVariable = 5
-
     const port = process.argv[2] || 3000;
     app.listen(port, () => {
       console.log(`Server started on port ${port}`);
